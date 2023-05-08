aa9729caa91e0f498a82ec3a999c2dc37b43cd17
Akshaykrishna4216 <129931476+Akshaykrishna4216@users.noreply.github.com>
2023-05-08 09:34:31 +0530

    Add files via upload

---
+++ EmpLinq/EmpLinq/Emp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpLinq
+{
+    public class Emp
+    {
+        private string _name;
+        public string Name { get { return _name; } set { value = _name; } }
+
+        private string _city;
+        public string City { get { return _city; } set { value = _city; } }
+        public Emp() { }
+        public Emp(string name, string city)
+        {
+            _name = name;
+            _city = city;
+        }
+
+    }
+}
---
+++ EmpLinq/EmpLinq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpLinq
+{
+   public class program
+    { 
+        static void Main(string[] args)
+        {
+            List<Emp> list = new List<Emp>();
+            list.Add(new Emp("akshay", "kerala"));
+            list.Add(new Emp("abi", "karnataka"));
+            list.Add(new Emp("anu", "tamil nadu"));
+            list.Add(new Emp("ram", "hyderabad"));
+            list.Add(new Emp("yadu", "mumbai"));
+            list.Add(new Emp("ajay", "mumbai"));
+            var empquery =
+                from Emp emp in list
+                orderby emp.City
+                group emp by emp.City;
+
+            foreach (var f in empquery)
+            {
+                Console.WriteLine("employees with " + f.Key + " city");
+
+                foreach (var e in f)
+                {
+                    Console.WriteLine(" "+e.Name);
+                }
+            }
+
+
+        }
+    }
+}
+
